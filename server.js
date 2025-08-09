2858a77f69a9b8551b3e4418f4103657c5dda6b4
Abhishek Gupta <agabhishek20121998@gmail.com>
2025-08-08 23:17:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const cors = require("cors");
-require("./cron/cronJob");
+// require("./cron/cronJob");
 const path = require("path");
 
 // Middleware
